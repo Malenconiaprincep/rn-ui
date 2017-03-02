b07bf418162321e4bc62fe42c60b9370c34f9464
makuta <wangbo@wepiao.com>
2017-03-02 16:04:49 +0800

    init

---
+++ index.ios.js
@@ -0,0 +1,54 @@
+import React, { Component, PropTypes } from 'react';
+import { AppRegistry, NavigatorIOS, Text, View, TouchableHighlight } from 'react-native';
+
+export default class NavigatorIOSApp extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        initialRoute={{
+          component: MyScene,
+          title: 'My Initial Scene',
+          passProps: { myProp: 'foo' }
+        }}
+        style={{flex: 1}}
+      />
+    );
+  }
+}
+
+class MyScene extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    navigator: PropTypes.object.isRequired,
+  }
+
+  constructor(props, context) {
+    super(props, context);
+    this._onForward = this._onForward.bind(this);
+    this._onBack = this._onBack.bind(this);
+  }
+
+  _onBack() {
+    this.props.navigator.pop()
+  }
+
+  _onForward() {
+    this.props.navigator.push({
+      title: 'Scene ' + nextIndex,
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Current Scene: { this.props.title }</Text>
+        <TouchableHighlight onPress={this._onForward}>
+          <Text>Tap me to load the next scene</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+// App registration and rendering
+AppRegistry.registerComponent('AwesomeProject', () => NavigatorIOSApp);
